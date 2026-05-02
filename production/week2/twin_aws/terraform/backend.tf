4e85acbcaeab5afeca3891f3b5e5f3bcbe87deff
Vedanth V Baliga <vedanthvbaliga@gmail.com>
2026-05-02 10:36:11 +0000

    [config] update backend tf

--- production/week2/twin_aws/terraform/backend.tf
+++ production/week2/twin_aws/terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    # These values will be set by deployment scripts
+    # These values will be set by deployment scripts.
     # For local development, they can be passed via -backend-config
   }
 }
