7303d4dee79a594775ed7c17dbefea3ed5ee9c29
Vedanth V Baliga <vedanthvbaliga@gmail.com>
2026-05-02 10:53:48 +0000

    [config] lock false config

--- production/week2/twin_aws/scripts/destroy.sh
+++ production/week2/twin_aws/scripts/destroy.sh
@@ -74,9 +74,9 @@ fi
 
 # Run terraform destroy with auto-approve
 if [ "$ENVIRONMENT" = "prod" ] && [ -f "prod.tfvars" ]; then
-    terraform destroy -var-file=prod.tfvars -var="project_name=$PROJECT_NAME" -var="environment=$ENVIRONMENT" -auto-approve
+    terraform destroy -var-file=prod.tfvars -var="project_name=$PROJECT_NAME" -var="environment=$ENVIRONMENT" -lock=false -auto-approve
 else
-    terraform destroy -var="project_name=$PROJECT_NAME" -var="environment=$ENVIRONMENT" -auto-approve
+    terraform destroy -var="project_name=$PROJECT_NAME" -var="environment=$ENVIRONMENT" -lock=false -auto-approve
 fi
 
 echo "✅ Infrastructure for ${ENVIRONMENT} has been destroyed!"
