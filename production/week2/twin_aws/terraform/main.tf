24816e0ed67c4a853b42997f08318bbc53a5df34
Vedanth V Baliga <vedanthvbaliga@gmail.com>
2026-05-02 09:50:38 +0000

    [config] update api name

--- production/week2/twin_aws/terraform/main.tf
+++ production/week2/twin_aws/terraform/main.tf
@@ -122,7 +122,7 @@ resource "aws_iam_role_policy_attachment" "lambda_s3" {
 # Lambda function
 resource "aws_lambda_function" "api" {
   filename         = "${path.module}/../backend/lambda-deployment.zip"
-  function_name    = "${local.name_prefix}-api"
+  function_name    = "${local.name_prefix}-api-123"
   role             = aws_iam_role.lambda_role.arn
   handler          = "lambda_handler.handler"
   source_code_hash = filebase64sha256("${path.module}/../backend/lambda-deployment.zip")
