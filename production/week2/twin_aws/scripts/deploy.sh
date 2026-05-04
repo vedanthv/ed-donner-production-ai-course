22953b1bd4184ad61c6c342ad31e06299901698d
Vedanth V Baliga <vedanthvbaliga@gmail.com>
2026-05-04 07:12:08 +0000

    [config] twin aws remove input false

--- production/week2/twin_aws/scripts/deploy.sh
+++ production/week2/twin_aws/scripts/deploy.sh
@@ -17,7 +17,7 @@ cd ..
 cd terraform
 AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
 AWS_REGION=${DEFAULT_AWS_REGION:-us-east-1}
-terraform init -input=false \
+terraform init \
   -backend-config="bucket=twin-terraform-state-${AWS_ACCOUNT_ID}" \
   -backend-config="key=${ENVIRONMENT}/terraform.tfstate" \
   -backend-config="region=${AWS_REGION}" \
