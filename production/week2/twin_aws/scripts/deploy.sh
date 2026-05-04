8b655af6840235b6294ba2decaaca168acce542a
Vedanth V Baliga <vedanthvbaliga@gmail.com>
2026-05-04 07:22:58 +0000

    [config] migrate state flag

--- production/week2/twin_aws/scripts/deploy.sh
+++ production/week2/twin_aws/scripts/deploy.sh
@@ -17,13 +17,14 @@ cd ..
 cd terraform
 AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
 AWS_REGION=${DEFAULT_AWS_REGION:-us-east-1}
-terraform init -input=true \
+terraform init \
+  -migrate-state \
   -backend-config="bucket=twin-terraform-state-${AWS_ACCOUNT_ID}" \
   -backend-config="key=${ENVIRONMENT}/terraform.tfstate" \
   -backend-config="region=${AWS_REGION}" \
   -backend-config="dynamodb_table=twin-terraform-locks" \
   -backend-config="encrypt=true"
-
+  
 if ! terraform workspace list | grep -q "$ENVIRONMENT"; then
   terraform workspace new "$ENVIRONMENT"
 else
